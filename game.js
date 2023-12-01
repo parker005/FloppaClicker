0a48b7470428a998c639163a5029552c0b2186bb
ben <90803531+pneb005@users.noreply.github.com>
2023-11-30 22:59:02 -0500

    Update game.js

--- game.js
+++ game.js
@@ -112,7 +112,7 @@ function rewardAch(number, achID){
     let rewardText = document.getElementById('reward');
     rewardText.innerHTML= msg;
     uList.push(aList[achID]); 
-    uList.join("<br>");
+    let achList = uList.join("<br>");
 
     let achElement = document.getElementById('achievements');
     
